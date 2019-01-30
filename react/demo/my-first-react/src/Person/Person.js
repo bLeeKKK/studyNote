f5f0ea38497b14066a6d1c81128c45d8e8288178
B.Lee <1961774471@qq.com>
2019-01-30 18:16:31 +0800

    react

--- react/demo/my-first-react/src/Person/Person.js
+++ react/demo/my-first-react/src/Person/Person.js
@@ -2,13 +2,19 @@ import React from "react";
 import "./Person.css";
 const person = ( props )=>{
     // props 可以为任何名字
+    const style = {
+        backgroundColor: "rgb(179, 119, 119)"
+    }
+
     return (
         <div className="Person">
             <p onClick={props.myClick}>{props.name?props.name:"默认"}的Person组件 第{props.count?props.count:"默认"}个</p>
             <p>
                 {props.children}
             </p>
-            <input onChange={props.changed} defaultValue={props.name}/>
+            <input  onChange={props.changed} 
+                    style = {style}
+                    defaultValue={props.name}/>
         </div>
     )
 };
